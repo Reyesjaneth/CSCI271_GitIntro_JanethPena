33e0e283d5125feeeff62d5b8f625811fa253564
Reyesjaneth <janeth1019889@my.yosemite.edu>
2026-05-12 17:16:39 -0700

    Add files via upload

---
+++ hello.cpp
@@ -0,0 +1,11 @@
+// CSCI 27X - GitHub Upload Practice
+// Name:Janeth
+// Section: 270-5132
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello from GitHub! My first commit works." << endl;
+    return 0;
+}
